3cc3b73255630758c8a19ddc24dfcd5402d4e2c0
Ardian-Hoti <ah54088@ubt-uni.net>
2024-01-19 15:04:09 +0100

    feat: add Newsletter section and Footer

--- script.js
+++ script.js
@@ -4,28 +4,54 @@ document.addEventListener("DOMContentLoaded", function () {
    const nextButton = document.querySelector(".next-button");
    const itemWidth = document.querySelector(".slider-item").offsetWidth;
 
-   let currentIndex = 0;
+   // let currentIndex = 0;
 
-   function updateSlider() {
-      const translateValue = -currentIndex * itemWidth;
-      wrapper.style.transform = `translateX(${translateValue}px)`;
-   }
+   // function updateSlider() {
+   //    const translateValue = -currentIndex * itemWidth;
+   //    wrapper.style.transform = `translateX(${translateValue}%)`;
+   // }
 
-   function prevSlide() {
-      if (currentIndex > 0) {
-         currentIndex--;
-         updateSlider();
-      }
-   }
+   // function prevSlide() {
+   //    if (currentIndex > 0) {
+   //       currentIndex--;
+   //       updateSlider();
+   //    }
+   // }
 
-   function nextSlide() {
-      const totalItems = document.querySelectorAll(".slider-item").length;
-      if (currentIndex < totalItems - 3) {
-         currentIndex++;
-         updateSlider();
-      }
-   }
+   // function nextSlide() {
+   //    const totalItems = document.querySelectorAll(".slider-item").length;
+   //    if (currentIndex < totalItems - 3) {
+   //       currentIndex++;
+   //       updateSlider();
+   //    }
+   // }
 
    prevButton.addEventListener("click", prevSlide);
    nextButton.addEventListener("click", nextSlide);
 });
+
+let currentIndex = 0;
+const slider = document.querySelector(".slider-wrapper");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+const itemWidth = document.querySelector(".slider-item").offsetWidth;
+
+function showSlide(index) {
+   currentIndex = index;
+   const translateValue = -index * 33  + "%";
+   slider.style.transform = `translateX(${translateValue})`;
+}
+
+function nextSlide() {
+   if (currentIndex < slider.children.length - 3) {
+      showSlide(currentIndex + 1);
+   }
+}
+
+function prevSlide() {
+   if (currentIndex > 0) {
+      showSlide(currentIndex - 1);
+   }
+}
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
